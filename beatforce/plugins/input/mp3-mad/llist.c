a0beeffc0c209949d62eb9e418452ecdec029de6
beuving <beuving>
2004-01-23 19:21:22 +0000

    Re-added files

---
+++ beatforce/plugins/input/mp3-mad/llist.c
@@ -0,0 +1,140 @@
+/*
+  Beatforce/ Linked list implementation
+
+  Copyright (C) 2003 John Beuving (john.beuving@home.nl)
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "llist.h"
+
+
+BFList*
+LLIST_Combine(BFList *list1,BFList *list2)
+{
+    BFList *temp;
+
+    if(list1)
+    {
+        temp=list1;
+        
+        while(temp->next)
+            temp=temp->next;
+        
+        
+        temp->next=list2;
+        return list1;
+    }
+    else
+        return list2;
+}
+
+BFList*
+LLIST_Prepend (BFList   *list,
+		 void* data)
+{
+  BFList *new_list;
+
+  new_list = malloc(sizeof(BFList));
+  new_list->data = data;
+  new_list->next = list;
+  new_list->prev = NULL;
+  return new_list;
+}
+
+BFList*
+LLIST_Append (BFList *list,void* data)
+{
+    BFList *new_list;
+    BFList *last;
+
+    new_list = malloc (sizeof(BFList));
+    
+    new_list->data = data;
+    new_list->next = NULL;
+    new_list->prev = NULL;
+
+    if (list)
+    {
+        last = LLIST_Last (list);
+        last->next = new_list;
+        new_list->prev = last;
+        new_list->next = NULL;
+        return list;
+    }
+    else
+    {
+        return new_list;
+    }
+}
+
+BFList*
+LLIST_Last (BFList *list)
+{
+    if (list)
+    {
+        while (list->next)
+            list = list->next;
+    }
+    return list;
+}
+
+BFList*
+LLIST_Remove (BFList *list, void* data)
+{
+    BFList *tmp;
+  
+    tmp = list;
+    while (tmp)
+    {
+        if (tmp->data != data)
+            tmp = tmp->next;
+        else
+        {
+            if (tmp->prev)
+                ((BFList*)(tmp->prev))->next = tmp->next;
+            if (tmp->next)
+                ((BFList*)(tmp->next))->prev = tmp->prev;
+            
+            if (list == tmp)
+                list = list->next;
+
+            free(tmp);
+            break;    
+        }
+    }
+    return list;
+}
+
+int
+LLIST_NoOfEntries(BFList *list)
+{
+    BFList *tmp;
+    int count=0;
+
+    tmp=list;
+    while(tmp)
+    {
+        count++;
+        tmp=tmp->next;
+    }
+    return count;
+}
+
+
+
+
---
+++ beatforce/plugins/input/ogg/debug.c
@@ -0,0 +1,104 @@
+/*
+  Beatforce/Debug
+
+  one line to give the program's name and an idea of what it does.
+  Copyright (C) 2003 John Beuving (john.beuving@home.nl)
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+char module_id[240];
+char msg[40];
+
+int IGR_Write_to_log(char* id,char* text)
+{
+    unsigned char Log_string[300];
+   
+    sprintf(Log_string,"%s%s\r\n",text,id);
+    printf("%s",Log_string);
+    return 0;
+}
+
+void noprint(char *fmt,...)
+{
+
+}
+
+void traceprintf(char *fmt,...)
+{
+    va_list ap;
+    int d,i=0,j=0;
+    char *s;
+    char tempbuf[200];
+    char tmpbuf[200];
+    char getal[10];
+
+    if(strlen(fmt)>200)
+        return;
+    va_start(ap, fmt);
+    while (*fmt)
+    {
+        switch (*fmt) 
+        {
+        case '%':
+            if(*fmt++)
+            {
+                switch(*fmt++)
+                {
+                case 's':  
+                    /* string */
+                    s=va_arg(ap, char *);
+                    if(s != NULL)
+                    {
+                        tempbuf[i++]='<';     /* will be set between < and > */
+                        while(*s)
+                            tempbuf[i++]=*s++;
+                        tempbuf[i++]='>';
+                    }
+                    break;
+                case 'd':                       /* int */
+                    d = va_arg(ap, int);
+                    sprintf(getal,"%d",d);
+                    while(getal[j])
+                        tempbuf[i++]=getal[j++];
+                    j=0;
+                    break;
+                }
+                break;
+            default:
+                tempbuf[i++]=*fmt++;
+                break;
+            }
+        }
+    }
+    va_start(ap, fmt);
+    tempbuf[i]=0;
+    sprintf(tmpbuf,"%s - %s",msg,tempbuf);
+    IGR_Write_to_log(module_id,tmpbuf);
+
+}
+
+void printid(char *id,int line,char *message)
+{
+    sprintf(msg,"%s",message);
+    sprintf(module_id,"\t%s (line %04d)",id,line);
+}
+
+
+
